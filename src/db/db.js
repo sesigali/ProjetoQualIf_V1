cce21c0775c14af77f02688040b963818a7ef74d
Deborafsilva <debora.fsilva@hotmail.com>
2024-05-03 16:49:30 -0300

    alteração senha do db

--- src/db/db.js
+++ src/db/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 
 // Configurar a conexão com o banco de dados
-const sequelize = new Sequelize("projeto_qualif", "root", "admin", {
+const sequelize = new Sequelize("projeto_qualif", "root", "", {
     host: "localhost",
     dialect: "mysql",
     logging: false
